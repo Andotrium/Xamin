789e1ff25f4396004d31561b23267c5f10c61e28
Pratham <59961114+Andotrium@users.noreply.github.com>
2024-08-16 14:49:15 +0530

    mobile header functionality

--- frontend/src/pages/mobile/mobileheader.js
+++ frontend/src/pages/mobile/mobileheader.js
@@ -1,30 +1,73 @@
 import { useEffect, useState } from "react"
 import "./mobileheader.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons"
 import { Link } from "react-router-dom"
+import { Accordion, AccordionItem, AccordionButton } from "@chakra-ui/react"
+import { click } from "@testing-library/user-event/dist/click"
+import { useLocation } from "react-router-dom"
 
-export default function MobileHeader(){
-    const [scroll,setscroll] = useState(false)
-    useEffect(()=>{
-        if(typeof window!=='undefined') {
-            window.addEventListener('scroll',()=>{
+export default function MobileHeader() {
+    const [scroll, setscroll] = useState(false)
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', () => {
                 setscroll(window.scrollY > 200)
             })
         }
-    },[])
 
-    return(
-        <div className={scroll?"mobilescrollheader":"mobileheader"}
-        >
+    }, [])
+    const [dropdown, setdropdown] = useState(false)
+    const location = useLocation();
+    useEffect(()=>{
+        setdropdown(false)
+    },[location])
+
+    const clicked = () => {
+        setdropdown(!dropdown)
+    }
+
+    return (
+        <div className={dropdown ? "dropdown" : (scroll ? "mobilescrollheader" : "mobileheader")}>
             <nav>
-                <Link to={'/'} style={{textDecoration:'none'}}>
-            <h1>Xamin</h1>
+                <Link to={'/'} style={{ textDecoration: 'none' }}>
+                    {dropdown ? "" : <h1>Xamin</h1>}
                 </Link>
             </nav>
-            {/* <FontAwesomeIcon style={{ color: "white", fontSize: '1.3em' }} icon={faBars} /> */}
-            <FontAwesomeIcon style={{color:"white", fontSize: '1.3em', margin:"20px"}} icon={faBars} />
-            
+            {
+                dropdown ?
+                    <div id="menu" >
+                        <div id="menuhead">
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'row' }}>
+                            <div id="menutext">
+                                <nav>
+                                    <Link to={'/about'} style={{ textDecoration: 'none' }}><h2>About us</h2></Link>
+                                </nav>
+                                <nav>
+                                    <Link to={'products'} style={{ textDecoration: 'none' }}><h2>Our Products</h2></Link>
+                                </nav>
+                                <nav>
+                                    <Link to={'contacts'} style={{ textDecoration: 'none' }}><h2>Contact Us</h2></Link>
+                                </nav>
+                                <nav>
+                                    <Link to={'downloads'} style={{ textDecoration: 'none' }}><h2>Downloads</h2></Link>
+                                </nav>
+                                <nav>
+                                    <Link to={'updates'} style={{ textDecoration: 'none' }}><h2>Updates</h2></Link>
+                                </nav>
+                            </div>
+                            <div id="rightblock">
+                            </div>
+
+                        </div>
+                    </div> :
+                    ""
+            }
+            <div id="bars">
+                <FontAwesomeIcon onClick={clicked} style={{ color: "white", fontSize: '1.3em', margin: "20px" }} icon={dropdown ? faX : faBars} />
+            </div>
         </div>
     )
 }
